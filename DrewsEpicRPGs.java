560cc5ef3a373917858ced360fab29bf4a03a63f
Gymnophoria <buddercreeperman@gmail.com>
2018-09-20 10:32:30 -0700

    finalize health theoretically

--- DrewsEpicRPGs.java
+++ DrewsEpicRPGs.java
@@ -22,7 +22,9 @@ class DrewsEpicRPGs {
     
     System.out.println("\n    What would you like to do?");
     System.out.println("\tinfo - get info about an RPG");
-    System.out.println("\tplay - play an RPG\n");
+    System.out.println("\tplay - play an RPG");
+    System.out.println("\ttest - test whatever code you currently feel like testing");
+    System.out.println();
     
     String input = scanner.nextLine();
     
@@ -30,6 +32,8 @@ class DrewsEpicRPGs {
     else if (input.toLowerCase().equals("play")) {
       System.out.println("WIP, returning to menu..");
       menu(scanner);
+    } else if (input.toLowerCase().equals("test")) {
+      test(scanner);
     } else menu(scanner);
     
   }
@@ -45,6 +49,31 @@ class DrewsEpicRPGs {
       System.out.println("Returning to menu..");
       menu(scanner);
     } else info(scanner);
+    
+  }
+  
+  private static void test (Scanner scanner) {
+    /* Dragon dragon = new Helpers.Dragon(100, 20, 50);
+      
+      System.out.println("New Dragon with " + dragon.getHealth() + " health. Watch it flap its wings..");
+      dragon.flapWings(); */
+    
+    System.out.println("Example health value please thanks");
+    double hp = scanner.nextDouble();
+    
+    Health health = new Health(hp);
+    health.displayHealthIfChanged();
+    health.displayHealthIfChanged();
+    System.out.println("hi");
+    health.takeDamage(10);
+    System.out.println("test");
+    health.displayHealthIfChanged();
+    System.out.println("mom");
+    health.displayHealthIfChanged();
+    health.displayHealthIfChanged();
+    health.displayHealthIfChanged();
+    health.displayHealthIfChanged();
+    health.displayHealthIfChanged();
     
   }
   
